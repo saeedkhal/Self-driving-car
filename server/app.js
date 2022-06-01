cc79965d5251730cab78a66f285025180ad870b6
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2022-06-01 10:59:39 +0200

    fix: return to main functionality

--- server/app.js
+++ server/app.js
@@ -1,70 +1,20 @@
-const fs = require('fs');
-const argv = require('minimist')(process.argv.slice(2));
 const { EventEmitter } = require('events');
 const { Server } = require('ws');
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
 const emitter = new EventEmitter();
 let server = express();
 const { spawn } = require('child_process');
-const python = spawn('python3', [__dirname, 'app.py']);
+const python = spawn('python', ['app.py']);
 const { logger } = require('./logger');
-
 server.use(cors());
-
 server = server.listen(process.env.PORT || 80, () =>
   logger('INFO', 'Server', `Server started on port ${process.env.PORT || 80}`),
 );
-
-const ip = argv.ip || '';
-const port = argv.port || '';
-let takeScreenshot;
-
-if (!ip || !port) {
-  logger('ERROR', 'Server', 'Missing ip or port');
-  process.exit(1);
-}
-
-const feedURL = `http://${ip}:${port}/shot.jpg`;
-
 let mode = 'pilot';
-
-let options = {
-  responseType: 'arraybuffer',
-  headers: {
-    Accept: 'image/jpg',
-  },
-};
-
-const sendImage = (image) => {
-  python.stdin.write(image, (error) => {
-    if (error) {
-      logger('ERROR', 'Server', 'Error sending image to python');
-    }
-  });
-};
-
-function startAutoPilot() {
-  takeScreenshot = setInterval(() => {
-    axios
-      .get(feedURL, options)
-      .then((response) => {
-        const image = new Buffer.from(response.data);
-        sendImage(image);
-        logger('INFO', 'Server', `Direction: ${direction}`);
-      })
-      .catch((err) => {
-        logger('ERROR', 'Server', err);
-      });
-  }, 1000);
-}
-
 const wss = new Server({ server });
-
 let chip = null;
 const devices = [];
-
 emitter.on('chipCall', (data) => {
   devices.forEach((device) => {
     try {
@@ -75,7 +25,6 @@ emitter.on('chipCall', (data) => {
     }
   });
 });
-
 emitter.on('sendDirections', (direction) => {
   if (chip) {
     chip.send(direction);
@@ -102,7 +51,7 @@ wss.on('connection', function connection(ws, req) {
       ws.on('message', function incoming(message) {
         logger('DATA', 'Master', message);
         try {
-          emitter.emit('chipCall', message);
+          readings = JSON.parse(message);
         } catch (error) {
           logger('ERROR', 'Master Data', 'Parsing JSON Data');
         }
@@ -117,13 +66,11 @@ wss.on('connection', function connection(ws, req) {
           mode = 'auto-pilot';
           emitter.emit('sendMode', 'auto-pilot');
           logger('INFO', 'Slave', 'Auto Pilot Mode');
+          startAutoPilot();
         } else if (message === 'pilot') {
           mode = 'pilot';
           emitter.emit('sendMode', 'pilot');
           logger('INFO', 'Slave', 'Pilot Mode');
-          try {
-            clearInterval(takeScreenshot);
-          } catch {}
         } else {
           logger('INFO', 'Slave', 'Send Pilot Direction');
           emitter.emit('sendDirections', message);
@@ -132,19 +79,18 @@ wss.on('connection', function connection(ws, req) {
       break;
   }
 });
-
 wss.on('error', (error) => {
-  logger('ERROR', 'Server', error);
+  logger('ERROR', 'Server', `${error}`);
 });
-
-python.stdout.on('data', (direction) => {
-  emitter.emit('sendDirections', direction);
+python.stdout.on('data', (data) => {
+  logger('INFO', 'Server', data.toString());
+  if (mode === 'auto-pilot') {
+    emitter.emit('sendDirections', data.toString());
+  }
 });
-
 python.stderr.on('data', (data) => {
   logger('ERROR', 'Child Process', data.toString());
 });
-
 python.on('close', () => {
   logger('WARNING', 'Child Process', 'Python Closed');
 });
